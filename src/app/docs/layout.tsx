abd132f28f5c2624b301bc64443bde8e6c422617
Oliver <oschi1303@gmail.com>
2025-11-28 19:15:08 +0100

    Organize tabs

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -4,7 +4,28 @@ import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout
+        tree={source.pageTree} {...baseOptions()}
+        sidebar={{
+            tabs: [
+                {
+                    title: 'Minecraft Plugins',
+                    description: 'Resources for our Minecraft (paper) plugins.',
+                    url: '/docs/minecraft-plugins',
+                },
+                {
+                    title: 'Hytale Plugins',
+                    description: 'Resources for our server side Hytale plugins.',
+                    url: '/docs/hytale-plugins',
+                },
+                {
+                    title: 'FancyAnalytics',
+                    description: 'Resources for our analytics platform FancyAnalytics.',
+                    url: '/docs/fancyanalytics',
+                },
+            ],
+        }}
+    >
       {children}
     </DocsLayout>
   );
